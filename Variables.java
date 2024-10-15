6f3dbbe4b6bf5c8ac6000840231edbcae29232d1
Loreto <lgonzalezc@iesch.org>
2024-10-15 18:01:26 +0200

    Added comments

--- Variables.java
+++ Variables.java
@@ -2,6 +2,7 @@ package ud1;
 
 public class variables {
     public static void main(String[] args) {
+        //Interchanging variables a and b
         int a = 7;
         int b = 2;
         a = a + b;
