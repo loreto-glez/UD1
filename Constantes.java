733a3f25e77291e3dfaa24e8548152f3d9e5c2b6
Loreto <lgonzalezc@iesch.org>
2024-10-15 18:18:53 +0200

    Creamos Constantes.java

---
+++ Constantes.java
@@ -0,0 +1,15 @@
+package ud1;
+
+public class Constantes {
+
+    public static final double G = 9.80665;
+
+    public static void main(String[] args) {
+        
+        double m = .450;
+        double f = m * G;
+        System.outprintln("La tierra atrae a un balón de fútbol con una fuerza de " + f + " newtons.")
+
+
+    }
+}
