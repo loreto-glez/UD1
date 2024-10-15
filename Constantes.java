7ba04c85b97573dd5d948f0c753e934e60dc1c95
Loreto <lgonzalezc@iesch.org>
2024-10-15 18:22:18 +0200

    Añadidos comentarios en Constantes

--- Constantes.java
+++ Constantes.java
@@ -1,11 +1,12 @@
 package ud1;
 
 public class Constantes {
-
+    // Fuerza de la Gravedad g
     public static final double G = 9.80665;
 
     public static void main(String[] args) {
-        
+        // Con qué fuerza atrae la tierra a un balón de fútbol
+        // Fórmula: F = m * g;
         double m = .450;
         double f = m * G;
         System.outprintln("La tierra atrae a un balón de fútbol con una fuerza de " + f + " newtons.")
